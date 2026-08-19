27309ac9a1ac1143491fb4eb0ee6a75aaa35d5b8
Katargina <katrin-kate@ukr.net>
2026-08-20 00:38:07 +0200

    login page fixed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,18 @@
 import LoginForm from "@/components/auth/LoginForm/LoginForm";
 import Container from "@/components/common/Container/Container";
 import styles from "./page.module.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Вхід | Назва Проєкту",
+  description:
+    "Увійдіть до свого акаунта, щоб отримати доступ до всіх можливостей сервісу.",
+  openGraph: {
+    title: "Вхід | Назва Проєкту",
+    description:
+      "Увійдіть до свого акаунта, щоб отримати доступ до всіх можливостей сервісу.",
+  },
+};
 
 export default function LoginPage() {
   return (
